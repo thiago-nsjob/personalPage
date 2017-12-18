1816fbdd6f634e6a67d65cdcc8ede82c67921278
tDog <thiago.nsjob@gmail.com>
2017-12-18 01:13:09 -0200

    layout

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+
+
+
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
+
+app.use(express.static(__dirname + '/app/view'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index.html');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
