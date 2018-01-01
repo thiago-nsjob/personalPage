57911263c4472ec3675d38059661bfa89181320a
thiago-nsjob <thiago.nsjob@gmail.com>
2018-01-01 21:38:11 -0200

    ajustes layout

---
+++ app/view/public/js/formatter.js
@@ -0,0 +1,39 @@
+function setHeaderBackground() {
+
+    var header = $('.headerBlock');
+    var pattern = Trianglify({
+        width: header.width() + (header.width() * 0.7),
+        height: header.height() + (header.height() * 0.7),
+        cell_size: 60,
+        stroke_width: 1.60,
+        x_colors: ["#011f4b", "#03396c", "#005b96", "#6497b1", "#b3cde0", "#be29ec", "#be29ec", "#800080", "#660066"]
+            //http://www.color-hex.com/color-palette/1294 + http://www.color-hex.com/color-palette/1835
+    });
+
+    header.css("background-image", "url(" + pattern.png() + ")");
+}
+
+function setFooterBackground() {
+
+    var footer = $('.page-footer');
+    var pattern = Trianglify({
+        width: footer.width() + (footer.width() * 0.7),
+        height: footer.height() + (footer.height() * 0.7),
+        cell_size: 60,
+        x_colors: ["#3a2458", "#573b7a", "#6b528b", "#8e71b4", "#b3cde0", "#be29ec", "#be29ec", "#800080", "#660066"]
+            //custom
+    });
+
+    footer.css("background-image", "url(" + pattern.png() + ")");
+}
+
+
+$(document).ready(function() {
+
+    $(window).resize(function() {
+        setFooterBackground();
+        setHeaderBackground();
+    });
+    setFooterBackground();
+    setHeaderBackground();
+});
